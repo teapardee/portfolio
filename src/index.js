3ba69e95864efe40e6b792ae746fb3d5c51b41e4
Tyler Pardee <tjpardee@gmail.com>
2023-10-16 10:14:03 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import Project from './modules/project.js';
+import Task from './modules/task';
+import {
+  addExpandListeners,
+  addEditListeners,
+  renderTask,
+  renderProject,
+  addSubmitListeners,
+  addInputListeners,
+} from './modules/dom.js';
+import { controller } from './modules/controller';
+import { storage } from './modules/storage';
