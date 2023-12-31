4c6222cc007355d7d125807c8fceb56e644cf417
Tyler Pardee <tjpardee@gmail.com>
2023-12-31 16:25:26 -0500

    Add Close Button Functionality

--- src/index.js
+++ src/index.js
@@ -7,20 +7,35 @@ function addEventListeners() {
       expandTile(e);
     });
   });
+  const close = document.querySelectorAll('.close-btn');
+  close.forEach(function (window) {
+    window.addEventListener('click', (e) => {
+      closeBtn(e);
+    });
+  });
 }
 
 function expandTile(e) {
-  console.log('test');
-
   let src = e.srcElement.attributes[0].nodeValue;
   let expandedContainer = document.getElementById('expanded-container');
   expandedContainer.style.display = 'flex';
   let tile = document.getElementById('expanded-tile');
 
   let img = document.createElement('img');
+
   img.src = `${src}`;
+  img.id = 'img';
 
   tile.appendChild(img);
+}
+
+function closeBtn(e) {
+  console.log(e);
+  let img = document.getElementById('img');
+  img.remove();
+  let id = e.target.parentElement.parentElement.id;
+  let grandParent = document.getElementById(id);
+  grandParent.style.display = 'none';
 }
 
 addEventListeners();
