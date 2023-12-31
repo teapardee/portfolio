f7be84d05c88cdf4da14617fb4f45800ce9c8966
Tyler Pardee <tjpardee@gmail.com>
2023-12-31 15:06:52 -0500

    Add Data Section & Event Listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+function addEventListeners() {
+  const expand = document.querySelectorAll('.expandable');
+  expand.forEach(function (window) {
+    window.addEventListener('click', (e) => {
+      expandTile(e);
+    });
+  });
+}
+
+function expandTile(e) {
+  console.log('test');
+
+  let src = e.srcElement.attributes[0].nodeValue;
+  let expandedContainer = document.getElementById('expanded-container');
+  expandedContainer.style.display = 'flex';
+  let tile = document.getElementById('expanded-tile');
+
+  let img = document.createElement('img');
+  img.src = `${src}`;
+
+  tile.appendChild(img);
+}
+
+addEventListeners();
